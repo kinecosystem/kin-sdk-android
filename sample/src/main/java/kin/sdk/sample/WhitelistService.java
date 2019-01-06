cb1d8ff47f07ab884c112b13a539aa8b9f2b4757
Amitai Efrati <amitai.efrati@gmail.com>
2019-01-06 15:55:11 +0200

    Update whitelist service url for the sample app

--- sample/src/main/java/kin/sdk/sample/WhitelistService.java
+++ sample/src/main/java/kin/sdk/sample/WhitelistService.java
@@ -21,7 +21,7 @@ import okhttp3.Response;
 
 class WhitelistService {
 
-    private static final String URL_WHITELISTING_SERVICE = "http://10.4.59.1:3003/whitelist";
+    private static final String URL_WHITELISTING_SERVICE = "http://34.239.111.38:3000/whitelist";
     private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
     private final OkHttpClient okHttpClient;
