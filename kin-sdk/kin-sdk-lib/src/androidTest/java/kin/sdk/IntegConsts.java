be9bf8b34b7131ecd34be5298f804cc612990662
Yossi Rizgan <yosriz@gmail.com>
2019-05-30 15:00:27 +0300

    change friendbot to our internal testing one

--- kin-sdk/kin-sdk-lib/src/androidTest/java/kin/sdk/IntegConsts.java
+++ kin-sdk/kin-sdk-lib/src/androidTest/java/kin/sdk/IntegConsts.java
@@ -5,7 +5,7 @@ final class IntegConsts {
 
     static final String TEST_NETWORK_URL = "https://horizon-testnet.kininfrastructure.com/";
     static final String TEST_NETWORK_ID = "Kin Testnet ; December 2018";
-    static final String URL_CREATE_ACCOUNT = "https://friendbot-testnet.kininfrastructure.com?addr=%s&amount=%d";
-    static final String URL_FUND = "https://friendbot-testnet.kininfrastructure.com/fund?addr=%s&amount="; // faucet
+    static final String URL_CREATE_ACCOUNT = "https://friendbot.developers.kinecosystem.com?addr=%s&amount=%d";
+    static final String URL_FUND = "https://friendbot.developers.kinecosystem.com/fund?addr=%s&amount="; // faucet
 
 }
