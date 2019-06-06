66e2ccd871efa1e986f64c28ecd874b497934d14
Amitai Efrati <amitai.efrati@gmail.com>
2019-06-06 15:58:21 +0300

    Add javadocs to our Transaction object and also remove the id/hash from the constructor.

--- kin-sdk/kin-sdk-lib/src/main/java/kin/sdk/Transaction.java
+++ kin-sdk/kin-sdk-lib/src/main/java/kin/sdk/Transaction.java
@@ -8,23 +8,21 @@ import kin.sdk.exception.DecodeTransactionException;
 
 public class Transaction {
 
-    /**
-     * The transaction hash
-     */
-    private final TransactionId id;
-
     private final kin.base.Transaction baseTransaction;
 
-    public Transaction(TransactionId id, kin.base.Transaction baseTransaction) {
-        this.id = id;
+    public Transaction(kin.base.Transaction baseTransaction) {
         this.baseTransaction = baseTransaction;
     }
 
+    /**
+     * Creates a <code>Transaction</code> instance from previously build <code>TransactionEnvelope</code>
+     *
+     * @param transactionEnvelope a Base-64 encoded <code>TransactionEnvelope</code>
+     */
     public static Transaction decodeTransaction(String transactionEnvelope) throws DecodeTransactionException {
         try {
             kin.base.Transaction transaction = kin.base.Transaction.fromEnvelopeXdr(transactionEnvelope);
-            TransactionId id = new TransactionIdImpl(Utils.byteArrayToHex(transaction.hash()));
-            return new Transaction(id, transaction);
+            return new Transaction(transaction);
         } catch (IOException e) {
             throw new DecodeTransactionException(e.getMessage(), e.getCause());
         }
@@ -35,6 +33,9 @@ public class Transaction {
         return baseTransaction.getSourceAccount();
     }
 
+    /**
+     * Returns fee paid for transaction in kin base unit (1 base unit = 0.00001 KIN).
+     */
     public int getFee() {
         return baseTransaction.getFee();
     }
@@ -43,23 +44,37 @@ public class Transaction {
         return baseTransaction.getMemo();
     }
 
+    /**
+     * return The transaction hash
+     */
     public TransactionId getId() {
-        return id;
+        return new TransactionIdImpl(Utils.byteArrayToHex(baseTransaction.hash()));
     }
 
     kin.base.Transaction getBaseTransaction() {
         return baseTransaction;
     }
 
+    /**
+     * see {@link WhitelistableTransaction} for more information on a whitelistable transaction</p>
+     */
     public WhitelistableTransaction getWhitelistableTransaction() {
         return new WhitelistableTransaction(baseTransaction.toEnvelopeXdrBase64(),
             Network.current().getNetworkPassphrase());
     }
 
+    /**
+     * Returns base64-encoded TransactionEnvelope object.
+     * Transaction need to have at least one signature.
+     */
     public String getTransactionEnvelope() {
         return baseTransaction.toEnvelopeXdrBase64();
     }
 
+    /**
+     * Adds a new signature to this transaction.
+     * @param account {@link KinAccount} object which is the account who we add his signature.
+     */
     public void addSignature(KinAccount account) {
         baseTransaction.sign(((KinAccountImpl) account).getKeyPair());
     }
