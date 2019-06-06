e6cbd3e21acedb612e39b25bfadb3fc13ff138ad
Amitai Efrati <amitai.efrati@gmail.com>
2019-06-06 15:21:53 +0300

    Indent the java doc

--- kin-sdk/kin-sdk-lib/src/main/java/kin/sdk/ControlledAccount.java
+++ kin-sdk/kin-sdk-lib/src/main/java/kin/sdk/ControlledAccount.java
@@ -3,7 +3,8 @@ package kin.sdk;
 /**
  * This class represents a controlled account. Controlled Account is an account that other account has also control over
  * him, for example, to send transactions with a payment operation on behalf of him. The account that has the control is
- * often called the master account. Master account can control more than one account and can sign for another.
+ * often called the master account.
+ * Master account can control more than one account and can sign for another.
  */
 public class ControlledAccount {
 
