381ba9777bf4e38da87c318e0a973bcfbc23abec
Amitai Efrati <amitai.efrati@gmail.com>
2019-07-10 08:11:43 +0300

    Handle PR issues

--- kin-backup-and-restore/kin-backup-and-restore-lib/src/androidTest/java/kin/backupandrestore/qr/QRFileUriHandlerImplTest.java
+++ kin-backup-and-restore/kin-backup-and-restore-lib/src/androidTest/java/kin/backupandrestore/qr/QRFileUriHandlerImplTest.java
@@ -12,27 +12,28 @@ import static org.hamcrest.core.IsNull.notNullValue;
 
 public class QRFileUriHandlerImplTest {
 
-	private QRFileUriHandlerImpl fileUriHandler;
+    private QRFileUriHandlerImpl fileUriHandler;
 
-	@Before
-	public void setup() {
-		fileUriHandler = new QRFileUriHandlerImpl(InstrumentationRegistry.getContext());
-	}
+    @Before
+    public void setup() {
+        fileUriHandler = new QRFileUriHandlerImpl(InstrumentationRegistry.getContext());
+    }
 
-	@Test
-	public void saveAndLoad() throws Exception {
-		Bitmap bitmap = TestUtils.loadBitmapFromResource(this.getClass(), "qr_test.png");
-		Uri uri = fileUriHandler.saveFile(bitmap);
-		Bitmap loadedBitmap = fileUriHandler.loadFile(uri);
-		assertThat(bitmap.sameAs(loadedBitmap), equalTo(true));
-	}
+    @Test
+    public void saveAndLoad() throws Exception {
+        Bitmap bitmap = TestUtils.loadBitmapFromResource(this.getClass(), "qr_test.png");
+        Uri uri = fileUriHandler.saveFile(bitmap);
+        Bitmap loadedBitmap = fileUriHandler.loadFile(uri);
+        assertThat(bitmap.sameAs(loadedBitmap), equalTo(true));
+    }
 
-	@Test
-	public void saveFile_Success() throws Exception {
-		Bitmap bitmap = TestUtils.loadBitmapFromResource(this.getClass(), "qr_test.png");
-		Uri uri = fileUriHandler.saveFile(bitmap);
-		assertThat(uri, notNullValue());
-		assertThat(uri.toString(), equalTo("content://kin.backupandrestore.test.KinRecoveryFileProvider/qr_codes/backup_qr.png"));
-	}
+    @Test
+    public void saveFile_Success() throws Exception {
+        Bitmap bitmap = TestUtils.loadBitmapFromResource(this.getClass(), "qr_test.png");
+        Uri uri = fileUriHandler.saveFile(bitmap);
+        assertThat(uri, notNullValue());
+        assertThat(uri.toString(), equalTo("content://kin.backupandrestore.test" +
+                ".KinRecoveryFileProvider/qr_codes/backup_qr.png"));
+    }
 
 }
