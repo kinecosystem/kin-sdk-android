9566cbaa12a62207c4f37bb11977f237203a7bc8
Yossi Rizgan <yosriz@gmail.com>
2019-07-11 14:00:47 +0300

    read integration tests network params from env variables

--- kin-sdk/kin-sdk-lib/src/androidTest/java/kin/sdk/IntegConsts.java
+++ kin-sdk/kin-sdk-lib/src/androidTest/java/kin/sdk/IntegConsts.java
@@ -3,9 +3,9 @@ package kin.sdk;
 
 final class IntegConsts {
 
-    static final String TEST_NETWORK_URL = "https://horizon-testnet.kininfrastructure.com/";
-    static final String TEST_NETWORK_ID = "Kin Testnet ; December 2018";
-    static final String URL_CREATE_ACCOUNT = "https://friendbot.developers.kinecosystem.com?addr=%s&amount=%d";
-    static final String URL_FUND = "https://friendbot.developers.kinecosystem.com/fund?addr=%s&amount="; // faucet
+    static final String TEST_NETWORK_URL = BuildConfig.INTEG_TESTS_NETWORK_URL;
+    static final String TEST_NETWORK_ID = BuildConfig.INTEG_TESTS_NETWORK_PASSPHRASE;
+    static final String URL_CREATE_ACCOUNT = BuildConfig.INTEG_TESTS_NETWORK_FRIENDBOT + "?addr=%s&amount=%d";
+    static final String URL_FUND = BuildConfig.INTEG_TESTS_NETWORK_FRIENDBOT + "/fund?addr=%s&amount="; // faucet
 
 }
