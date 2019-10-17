63fe4ac9cd0e006f7226830b363dde2a64180c89
Bryan Lahartinger <Bryan.Lahartinger@gmail.com>
2019-10-17 11:31:21 -0400

    No need for any of these to be env variables. Currently, TravisCI is injecting different - and broken - urls.

--- kin-sdk/kin-sdk-lib/src/androidTest/java/kin/sdk/IntegConsts.java
+++ kin-sdk/kin-sdk-lib/src/androidTest/java/kin/sdk/IntegConsts.java
@@ -3,15 +3,10 @@ package kin.sdk;
 
 final class IntegConsts {
 
-    static final String TEST_NETWORK_URL = BuildConfig.INTEG_TESTS_NETWORK_URL.isEmpty() ?
-            Environment.TEST.getNetworkUrl() : BuildConfig.INTEG_TESTS_NETWORK_URL;
-    static final String TEST_NETWORK_ID = BuildConfig.INTEG_TESTS_NETWORK_PASSPHRASE.isEmpty() ?
-            Environment.TEST.getNetworkPassphrase() : BuildConfig.INTEG_TESTS_NETWORK_PASSPHRASE;
-    static final String FRIENDBOT_URL = BuildConfig.INTEG_TESTS_NETWORK_FRIENDBOT.isEmpty() ?
-            "https://friendbot-testnet.kininfrastructure.com" : BuildConfig.INTEG_TESTS_NETWORK_FRIENDBOT;
+    static final String TEST_NETWORK_URL = Environment.TEST.getNetworkUrl(); // BuildConfig.INTEG_TESTS_NETWORK_URL.isEmpty() ? Environment.TEST.getNetworkUrl() : BuildConfig.INTEG_TESTS_NETWORK_URL;
+    static final String TEST_NETWORK_ID = Environment.TEST.getNetworkPassphrase(); // BuildConfig.INTEG_TESTS_NETWORK_PASSPHRASE.isEmpty() ? Environment.TEST.getNetworkPassphrase() : BuildConfig.INTEG_TESTS_NETWORK_PASSPHRASE;
+    static final String FRIENDBOT_URL =  "https://friendbot-testnet.kininfrastructure.com";  //BuildConfig.INTEG_TESTS_NETWORK_FRIENDBOT.isEmpty() ? "https://friendbot-testnet.kininfrastructure.com" : BuildConfig.INTEG_TESTS_NETWORK_FRIENDBOT;
     static final String URL_CREATE_ACCOUNT = FRIENDBOT_URL + "?addr=%s&amount=%d";
     static final String URL_FUND = FRIENDBOT_URL + "/fund?addr=%s&amount="; // faucet
-    static final String URL_WHITELISTING_SERVICE = (BuildConfig.INTEG_TESTS_NETWORK_FRIENDBOT.isEmpty() ?
-            "http://34.239.111.38:3000" : BuildConfig.INTEG_TESTS_NETWORK_FRIENDBOT)
-            + "/whitelist";
+    static final String URL_WHITELISTING_SERVICE = "http://34.239.111.38:3000/whitelist"; //(BuildConfig.INTEG_TESTS_NETWORK_FRIENDBOT.isEmpty() ? "http://34.239.111.38:3000" : BuildConfig.INTEG_TESTS_NETWORK_FRIENDBOT) + "/whitelist";
 }
